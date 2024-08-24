aece213c031527dfe432c04767f8893628eca143
Yağızhan <yagizhan49@protonmail.com>
2024-08-24 18:30:56 +0300

    Solution for Problem 2

---
+++ c/002.c
@@ -0,0 +1,35 @@
+/**
+ * Problem 2: Even Fibonacci Numbers
+ * https://projecteuler.net/problem=2
+ *
+ * github.com/evrifaessa
+ * yagiz.dev
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    int smaller = 1;
+    int bigger = 2;
+    int next;
+
+    int max = 4000000;
+    int sum = 0;
+
+    while (bigger < max)
+    {
+        if (bigger % 2 == 0)
+        {
+            sum = sum + bigger;
+        }
+
+        next = smaller + bigger;
+        smaller = bigger;
+        bigger = next;
+    }
+
+    printf("%d", sum);
+
+    return 0;
+}
